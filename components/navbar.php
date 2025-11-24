d5d544fce1118f17c8c7a91936fcb7f99405b14e
Michael Mikey <mjmichael155@gmail.com>
2025-11-24 13:59:47 +0100

    feat: add user login check to navbar for conditional rendering of links

--- components/navbar.php
+++ components/navbar.php
@@ -1,8 +1,13 @@
+<?php
+// Check if user is logged in by checking session
+$isLoggedIn = isset($_SESSION['user_id']) || isset($_SESSION['user']);
+?>
 <nav class="navbar">
     <div class="navbar-container">
         <div class="navbar-brand">
             <span class="brand-text">Gezondheids<span class="brand-meter">Meter</span></span>
         </div>
+        <?php if ($isLoggedIn): ?>
         <div class="navbar-links">
             <a href="#" class="nav-link">Dashboard</a>
             <a href="#" class="nav-link">Vragen</a>
@@ -10,5 +15,6 @@
             <a href="#" class="nav-link">Instellingen</a>
             <a href="#" class="nav-link">Account</a>
         </div>
+        <?php endif; ?>
     </div>
 </nav>
