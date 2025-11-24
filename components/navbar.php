4f1b33bb11d75e9a01aaba628e1818eb17e8ae5f
Michael Mikey <mjmichael155@gmail.com>
2025-11-24 13:21:10 +0100

    feat: implement navbar styling and structure for improved navigation

--- components/navbar.php
+++ components/navbar.php
@@ -1,1 +1,14 @@
-<div class="header"></div>
+<nav class="navbar">
+    <div class="navbar-container">
+        <div class="navbar-brand">
+            <span class="brand-text">Gezondheids<span class="brand-meter">Meter</span></span>
+        </div>
+        <div class="navbar-links">
+            <a href="#" class="nav-link">Dashboard</a>
+            <a href="#" class="nav-link">Vragen</a>
+            <a href="#" class="nav-link">Geschiedenis</a>
+            <a href="#" class="nav-link">Instellingen</a>
+            <a href="#" class="nav-link">Account</a>
+        </div>
+    </div>
+</nav>
