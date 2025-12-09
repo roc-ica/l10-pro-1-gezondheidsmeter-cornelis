f50c7a0446c6a6243406b57e22ff8ced20595a01
Kaj <589045@edu.rocmn.nl>
2025-12-09 10:27:43 +0100

    feat: Add user account page with profile management, User model, and admin users overview.

--- pages/account.php
+++ pages/account.php
@@ -20,7 +20,6 @@ if (!$user) {
 
 $email = htmlspecialchars($user->email);
 $username = htmlspecialchars($user->username);
-$birthdate = htmlspecialchars($user->birthdate);
 $gender = htmlspecialchars($user->gender);
 ?>
 <!DOCTYPE html>
@@ -60,7 +59,6 @@ $gender = htmlspecialchars($user->gender);
                 </div>
                 <div class="identity-info2">
                     <p><?= htmlspecialchars($email) ?></p>
-                    <p><?= htmlspecialchars($birthdate) ?></p>
                     <p><?= htmlspecialchars($gender) ?></p>
                 </div>
             </div>
